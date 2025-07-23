5779c1e4e225f69275ea134beafa2905698fd50b
saulo-duarte <saulolduarte@outlook.com>
2025-07-23 17:02:34 -0300

    feat: wrap children in Providers component for React Query integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import { Providers } from '@/providers/ReactQuery';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -25,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" className="dark">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased px-2 md:px-6`}>
-        {children}
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
